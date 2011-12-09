4a5179f1b5e06e84fe923de0011301fa5314c881
Carla Guillen <carlaguill@gmail.com>
2011-12-09 21:44:40 -0200

    adicionando zip com entrega final da p2

--- P2/main.c
+++ P2/main.c
@@ -17,44 +17,6 @@
 #include "table_of_symbols.h"
 #include "spa.h"
 
-/************************************************************/
-/*					  TESTE SEMÂNTICO						*/
-/************************************************************/
-
-//int main(int argc, char **argv) {
-//
-//	if(argc < 2) {
-//		printf("No input file to compile.");
-//	}
-//	char * filename = argv[1];
-//	init_file_reader(filename);
-//	init_file_writer(filename);
-//	init_token();
-//	init_transition_table();
-//	spa_init();
-//
-//
-//	get_next_token(); /* lexical analyser */
-//	while (token->type != TTYPE_END_OF_FILE) {
-//		if (token->type != TTYPE_COMMENT && token->type != TTYPE_STRING)
-//			if (!spa_step()) { /* sintatic analyser */
-//				fprintf(stderr, "source code could not be correctly parsed");
-//				break;
-//			}
-//		get_next_token(); /* lexical analyser */
-//
-//	}
-//
-//	close_file_writer();
-//	merge_code_data();
-//
-//	return 0;
-//}
-
-/************************************************************/
-/*					  TESTE SINTÁTICO						*/
-/************************************************************/
-
 int main(int argc, char **argv) {
 
 	init_file_reader("ENTRADA.k");
